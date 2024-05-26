ac01cb4ab96e5a568f6fb8a58b9fd0d480caf0c1
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2024-05-27 01:09:33 +0200

    get book by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,13 +29,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let book = Object.values(books).filter((book)=> book.author===author);
+  res.send(book)
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
